3ae0275b2d95b5e4a9cf1c5ccd7a55fa8c8cd56d
NguyenTram13 <ngoctrambu13012001@gmail.com>
2023-02-08 10:01:02 +0700

    Khoi tao du an

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    server: {
-      port: 3001,
-    },
+  },
+  server: {
+    port: 3001,
   },
 });
